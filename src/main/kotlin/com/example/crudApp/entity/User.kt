c53b1741a40196291cdde700f80079dec32be6bf
Tomasz Hancyk <mitosowy@gmail.com>
2025-06-26 16:06:59 +0200

    feat: add User entity

---
+++ src/main/kotlin/com/example/crudApp/entity/User.kt
@@ -0,0 +1,16 @@
+package com.example.crudApp.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    var email: String,
+    var password: String,
+    var isActive: Boolean,
+    var firstName: String,
+    var lastName: String
+)
