b055bf316b358e0db31f36ed9e7c94dc75bd7343
Tomasz Hancyk <mitosowy@gmail.com>
2025-06-28 00:37:38 +0200

    fix: update user entity

--- src/main/kotlin/com/example/crudApp/entity/User.kt
+++ src/main/kotlin/com/example/crudApp/entity/User.kt
@@ -8,8 +8,13 @@ data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
+
+    @Column(unique = true)
     var username: String,
+
+    @Column(unique = true)
     var email: String,
+
     var password: String,
     var isActive: Boolean,
     var firstName: String,
