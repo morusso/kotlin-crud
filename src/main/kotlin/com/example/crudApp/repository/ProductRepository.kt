a8b168e7f61f234cbd0639f26d0dc117eba12fdc
Tomasz Hancyk <mitosowy@gmail.com>
2025-06-29 23:36:18 +0200

    fix: add unique to name filed and update repository

--- src/main/kotlin/com/example/crudApp/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/crudApp/repository/ProductRepository.kt
@@ -1,9 +1,14 @@
 package com.example.crudApp.repository
 
 import com.example.crudApp.entity.Product
+import com.example.crudApp.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface ProductRepository : JpaRepository<Product, Long> {
+    fun findByName(name: String): Optional<Product>
+    fun existsByName(name: String): Boolean
+
 }
